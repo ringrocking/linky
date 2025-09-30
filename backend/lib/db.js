7bfd2e5bdfe5b584f365b55376e589adc2aeddbf
ringrocking <151378193+ringrocking@users.noreply.github.com>
2025-10-01 01:23:53 +0530

    second commit

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect("mongodb+srv://rjsingh4997:2CtDQ4RwsVwTYi6a@cluster0.mjrqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		const conn = await mongoose.connect("");
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error connecting to MongoDB: ${error.message}`);
